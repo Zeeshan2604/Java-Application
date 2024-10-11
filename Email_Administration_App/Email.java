c41ea036a6b0b5ec279ba33dc5b9afb2230ea335
Zeeshan Shaikh <zeeshanshaikhmega@gmail.com>
2024-10-11 14:39:14 +0530

    commit_2

--- Email_Administration_App/Email.java
+++ Email_Administration_App/Email.java
@@ -1,7 +1,8 @@
 
 import java.util.Scanner;
 
-public class Email{
+public class Email {
+
     private String firstName;
     private String lastName;
     private String password;
@@ -11,9 +12,8 @@ public class Email{
     private String alternateEmail;
     private String companySuffix = "txscompany.com";
 
-
     // Constructor to receive the first name and last name
-    public Email(String firstName, String lastName){
+    public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
 
@@ -22,32 +22,36 @@ public class Email{
 
         // Call a method to return a random password
         this.password = randomPassword(8);
-        System.out.println("Generated Password: "+ this.password);
+        System.out.println("Generated Password: " + this.password);
 
         // Combine elements to generate email
-        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department +"." + companySuffix;
-        
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+
     }
 
     // Ask for department
     private String setDepartment() {
-        System.out.println("New Employee: "+firstName+" - Department codes: 1 for IT | 2 for Sales | 3 for Accounting | 0 for none | Enter the dept. code: ");
+        System.out.println("New Employee: " + firstName + " - Department codes: 1 for IT | 2 for Sales | 3 for Accounting | 0 for none | Enter the dept. code: ");
         Scanner in = new Scanner(System.in);
         int depChoice = in.nextInt();
         return switch (depChoice) {
-            case 1 -> "IT";
-            case 2 -> "Sales";
-            case 3 -> "Accounting";
-            default -> "";
+            case 1 ->
+                "IT";
+            case 2 ->
+                "Sales";
+            case 3 ->
+                "Accounting";
+            default ->
+                "";
         };
-        
+
     }
 
     // Generate a random password
-    private String randomPassword(int length){
+    private String randomPassword(int length) {
         String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$";
         char[] password = new char[length];
-        for(int i=0;i<length;i++){
+        for (int i = 0; i < length; i++) {
             int rand = (int) (Math.random() * passwordSet.length());
             password[i] = passwordSet.charAt(rand);
 
@@ -56,29 +60,37 @@ public class Email{
     }
 
     // Set the mailbox capacity
-    public void setMailboxCapacity(int capacity){
+    public void setMailboxCapacity(int capacity) {
         this.mailboxCapacity = capacity;
     }
 
     // Set the alternate email
-    public void setAlternateEmail(String altEmail){
+    public void setAlternateEmail(String altEmail) {
         this.alternateEmail = altEmail;
     }
 
     // Change password
-    public void changePassword(String password){
+    public void changePassword(String password) {
         this.password = password;
     }
 
-    public int getMailboxCapacity(){ return mailboxCapacity;}
-    public String getAlternateEmail(){ return alternateEmail;}
-    public String getPassword(){ return password;}
-
-    public String showInfo(){
-        return "DISPLAY NAME: " + firstName + " " + lastName +
-                "\nCOMPANY EMAIL: " + email + 
-                "\nALT EMAIL: " + alternateEmail + 
-                "\nPASSWORD: " + password + 
-                "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+    public int getMailboxCapacity() {
+        return mailboxCapacity;
     }
-} 
+
+    public String getAlternateEmail() {
+        return alternateEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String showInfo() {
+        return "DISPLAY NAME: " + firstName + " " + lastName
+                + "\nCOMPANY EMAIL: " + email
+                + "\nALT EMAIL: " + alternateEmail
+                + "\nPASSWORD: " + password
+                + "\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+    }
+}
